64130600d3ec2462435ccbc75f3ecd437216ca2c
sofiya <sofiyaradzi@gmail.com>
2026-02-14 01:26:00 +0800

    update frontend to get real backend data

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -5,9 +5,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
 
   css: ['~/assets/css/main.css'],
-  
+
   tailwindcss: {
     cssPath: '~/assets/css/main.css',
     configPath: 'tailwind.config.js',
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
+    }
   }
 })
